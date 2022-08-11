2ec1614592ba269b54ee4a79666a3161331fc3c0
nadiavl <nadiavali.ae@gmail.com>
2022-08-11 15:44:41 +0200

    11

---
+++ exercises/ex4/exercise.py
@@ -0,0 +1,20 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+#task1
+n=0
+while n < 3:
+    x,y=input('num1:'),input('num2:')
+    if x > y:
+        print('num1 is greater!')
+    elif x<y:
+        print('num2 is greater!')
+    elif x==y:
+        print('num1 is equal to num2')
+    elif x>=y:
+        print('num1 is equal or greater than num2')
+
+    n += 1
+    
+
