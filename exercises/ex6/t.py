3895c23a98538309d8213142cebaa76b57ed764f
nadiavl <nadiavali.ae@gmail.com>
2022-08-17 11:28:21 +0200

    m

--- exercises/ex6/t.py
+++
@@ -1,9 +0,0 @@
-x = int(input("First number: "))
-y = int(input("Second number: "))
-
-if x %y == 0:
-    print("First number is divisible by second number, result =", x // y)
-elif y %x == 0:
-    print("Second number is divisible by first number, result =", y // x)
-else:
-    print("Numbers are non-divisible!")
