9dee49358ef67f968ea797eb5ba61203bf0113a8
nadiavl <nadiavali.ae@gmail.com>
2022-08-24 13:36:06 +0200

    m

---
+++ exercises/ex0.1/ex.1.0.py
@@ -0,0 +1,38 @@
+#task1
+
+text = '''Berlin is a world city of culture,
+politics, media and science.'''
+ 
+print(len(text))
+
+#task2
+
+text = '''Berlin is a world city of culture,
+politics, media and science.'''
+
+print(text[0], text[-1])
+
+#task3
+
+text = '''Berlin is a world city of culture,
+politics, media and science.'''
+
+print(text[0:3].upper())
+
+#task4
+
+text =  '''Berlin is surrounded by
+the State of Brandenburg and contiguous with
+Potsdam, Brandenburg's capital'''
+
+print('B appears', ':',  text.count('B'), 'times')
+
+#task5
+
+text = '''Berlin straddles the banks of the Spree,
+which flows into the Havel (a tributary of the
+Elbe) in the western borough of Spandau.'''
+
+print(text[-10:])
+
+
