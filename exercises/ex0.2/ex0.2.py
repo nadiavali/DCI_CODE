3dea1a982fc7a634e7193c00eaf23072f2463e95
nadiavl <nadiavali.ae@gmail.com>
2022-08-24 14:38:30 +0200

    m

---
+++ exercises/ex0.2/ex0.2.py
@@ -0,0 +1,12 @@
+
+secret =input('this is my secret') 
+love_name =input('your love name')
+birthday = input("your love's year of birth")
+
+t = secret + love_name 
+t = t[::-1]
+print( t + birthday)
+
+
+
+
