4aa2e5f2491b7cf2388ba835984702d6a523adb2
nadiavl <nadiavali.ae@gmail.com>
2022-08-25 14:19:18 +0200

    mm

--- exercises/ex5/ex5.0/ex5.0.py
+++ exercises/ex5/ex5.0/ex5.0.py
@@ -1,9 +1,8 @@
 def conCa(z):
   
-    if  z[-1] == 'a':
+    if  z[-1] == 'a' or z[-1] == 'e' or z[-1] =='i' or z[-1] =='u'or z[-1] =='o' :
      return z+'inator'+str(len(z))+'000'
      
-    else:
-        return z+'-inator'+str(len(z))+'000'
+    return z+'-inator'+str(len(z))+'000'
 
-print(conCa('manam'))
+print(conCa('manamt'))
