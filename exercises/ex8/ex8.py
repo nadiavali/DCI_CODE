ec6eef5b83c7bb697b71fec2379a2b2f718c9f68
nadiavl <nadiavali.ae@gmail.com>
2022-08-26 11:29:13 +0200

    m

--- exercises/ex8/ex8.py
+++ exercises/ex8/ex8.py
@@ -1,8 +1,8 @@
-
-text = input('write about Nemo or Timo!')
-
-a = text.find('Nemo')
-if a:
-   print('I found Nemo at' + str(a))
-else:
-    print("I can't find Nemo")
+def finding_Nemo(text):
+   if text.find('Nemo') != -1:
+      result = 'I found Nemo at' + str(text.find('Nemo'))
+   else:
+      result = "I can't find Nemo :("
+   return result
+print(finding_Nemo('I am Nemo'))
+print(finding_Nemo('I am Nadia'))
