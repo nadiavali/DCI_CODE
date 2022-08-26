cc67b9ed66cbbe39a7811ca44c6fbb9ab4dcded6
nadiavl <nadiavali.ae@gmail.com>
2022-08-26 13:01:02 +0200

    b

--- exercises/ex11/ex11.py
+++ exercises/ex11/ex11.py
@@ -1,4 +1,4 @@
 def f_replace(text):
-    text = f"{text.replace('dog','cat')}"
-    return text
+    result = f"{text.replace('dog','cat')}"
+    return result
 print(f_replace('A dogmatic dog buys dogecoin to become rich and buy hotdogs every day.'))
