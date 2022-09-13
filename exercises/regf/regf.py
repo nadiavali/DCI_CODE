52368e60815510f7b750c7bc21c4039b0c1d2c2d
vic <nadiavali.ae@gmail.com>
2022-09-13 13:37:45 +0200

    reg

--- exercises/regf/regf.py
+++ exercises/regf/regf.py
@@ -1,11 +1,44 @@
-from ast import pattern
+# from ast import pattern
+# import re
+# # Task1
+# def reg(text1):
+#     pattern = r"\s"
+#     location = re.search(pattern, text1)
+#     print(location)
+#     print(location.span()[0])
+
+# text1 ='Berlin is a world city of culture, politics, media and science.'
+# reg(text1)
+
+# Task2
+
+# import re
+
+# def regX (text2):
+#     pattern = r"Frankfurt"
+#     location = re.search(pattern, text2)
+#     print(location)
+
+# text2 = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+# regX(text2)
+
+# Task3
+# import re
+# def regX (text3):
+#     pattern = r"\s"
+#     location = re.sub(pattern, '-', text3)
+#     print(location)
+
+# text3 = "Berlin is a city of culture."
+# regX(text3)
+
+# Task4
+
 import re
-
-def reg(text1):
-    pattern = r"\s"
-    location = re.search(pattern, text1)
+def regX (text4):
+    pattern = r"\in"
+    location = re.search(pattern,  text4)
     print(location)
-    print(location.span()[0])
 
-text1 ='Berlin is a world city of culture, politics, media and science.'
-reg(text1)
+text4 = "Berlin is a city of culture."
+regX(text4)
