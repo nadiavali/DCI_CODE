46d35ef71ee4bfb412fe00f4e29902c5f026d38f
vic <nadiavali.ae@gmail.com>
2022-09-13 14:32:29 +0200

    mail

--- exercises/exreg/match.py
+++ exercises/exreg/match.py
@@ -1,15 +1,24 @@
-# import re module
-import re
- 
-Substring ='string'
- 
- 
-String1 ='''We are learning regex with geeksforgeeks
-         regex is very useful for string matching.
-          It is fast too.'''
-String2 ='''string We are learning regex with geeksforgeeks
-         regex is very useful for string matching.
-          It is fast too.'''
- 
-# Use of re.search() Method
-print(re.search(Substring, String1, re.IGNORECASE))
+
+import re 
+
+def mail(text):
+    pattern = r"\w+@\w+.[a-z]{2,3}"
+    location = re.findall(pattern,text)
+    for i in location:
+        print(i)
+
+
+text = """Hey Mr. Bezos,
+
+I hope its okay I message you in this unsecure email program. Sry about that!
+Here the list of extremely confidential clients and close coworkers of you who actually visited Epsteins island. Oh boy how I hope no random Python Students extract this sensitive list with some kind of regular expressions! 
+
+therealjeffbezos@bossnet.com
+markzuckerbergtheman@facebookormetaidkmail.com
+donaldtothatrump@getthatcapitolmail.com
+2pacaintdeadandnowchillinonwrongislands@mail.com
+
+Kind regards,
+Tanisha"""  
+mail(text)
+
