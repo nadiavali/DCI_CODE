98d80fea7e1f61e3da67b72fca7aa333d1af0613
vic <nadiavali.ae@gmail.com>
2022-09-14 13:20:09 +0200

    time

---
+++ exercises/timeex/time.py
@@ -0,0 +1,19 @@
+import  time, os
+from time import sleep
+
+sleep(1)
+print("------   The  --------")
+sleep(1)
+print("------   Game --------")
+sleep(1)
+print("------   Is   --------")
+sleep(1)
+print("------   Loading..... ")
+sleep(5)
+print("I just lied to you!  ")
+sleep(3)
+print(":))))))))")
+sleep(3)
+print("YOU ARE DEAD.........")
+sleep(3)
+print( 'Time of dead:', time.ctime() )
