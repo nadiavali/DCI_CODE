45f62acbbaffcd323ebd866455468f682d5b1fa2
vic <nadiavali.ae@gmail.com>
2022-09-15 11:07:35 +0200

    K

--- exercises/datetime/1datetime.py
+++ exercises/datetime/1datetime.py
@@ -8,3 +8,4 @@ from datetime import datetime
 date = "10 January 1993"
 converted_date = datetime.strptime(date, "%d %B %Y") # The date format and the format of strptime should be the same and the result will be as ISO 8601
 print(converted_date)
+
