02501633d046713557e55118546771e5e0bd407a
vic <nadiavali.ae@gmail.com>
2022-09-15 10:58:16 +0200

    date time

---
+++ exercises/datetime/1datetime.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+# print(datetime.today())
+
+# date_as_string = '2022-01-10'
+# print(date.fromisoformat(date_as_string))
+
+date = "10 January 1993"
+converted_date = datetime.strptime(date, "%d %B %Y") # The date format and the format of strptime should be the same and the result will be as ISO 8601
+print(converted_date)
