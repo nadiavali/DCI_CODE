96a276c13d610616d2de6ec2d923cac47e0b91b6
vic <nadiavali.ae@gmail.com>
2022-09-21 15:11:09 +0200

    na

---
+++ exercises/Birthday Calculator/birthday_calculator.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+n = str(input('your birthday! '))
+m = print(datetime.strftime(n,'%d-%B-%Y'))
+print(m)
--- exercises/datetime1/datetime1.py
+++ exercises/datetime1/datetime1.py
@@ -11,9 +11,10 @@ current_datetime =datetime.today()
 print(current_datetime + timedelta(days=7))
 
 # Task3
-
+import datetime
 start_date = str(input('When is the start date of yor rent? (in DD-MM-YYYY)'))
 name = input('what is your name?')
 price = input ('How much you pay?')
-convert_date =print(datetime.strftime(start_date,'%d-%B-%Y'))
-print('Hey' + name + 'your rent of' + price + '€' + 'is due on' + convert_date )
+convert_date =datetime.datetime.strptime(start_date, '%d-%m-%Y')
+m = convert_date.strftime('%d-%B-%Y')
+print('Hey' + name + 'your rent of' + price + '€' + 'is due on' , m)
