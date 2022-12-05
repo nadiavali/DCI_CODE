66c032cfb06e8fe06844c23269a17e2aa236496c
vic <nadiavali.ae@gmail.com>
2022-12-05 12:56:53 +0100

    flask

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,20 +1,33 @@
 # export FLASK_APP=flaskblog.py
 #flask run
-#export FLASK_DEBUG=1 #to can make as much change'as we need withot crl+c
+#export FLASK_DEBUG=1 #to can make as much change'as we need without crl+c
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+posts = [
+    {   "author": "Nadia",
+        "title": "Hello World",
+        "content": "This is my first blog post.",
+        "date_posted":'April 20, 2020'
+    },
+    {   "author": "Corey", 
+        "title": "Hello Nadia",
+        "content": "This is my second blog post.",
+        "date_posted":'April 22, 2020'
+    }
+
+]
 @app.route("/")
-@app.route("/home")
+#@app.route("/home") # two routs do the same thing
 def home():
-    return "<h1>This is a Header!</h1>"  # heading text in html 
-
-@app.route("/about")
+    return render_template('home.html', posts=posts)  # heading text in html 
+@app.route("/about") # each page and method you create you need to make a route first for that
 def about():
-    return "<h1>About!</h1>"
+    return render_template('about.html', title='About')
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': # this helps us to run it without exporting and running flask # Python3 flaskblog.py
     app.run(debug=True)
